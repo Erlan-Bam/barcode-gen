e6017013ba8a1ef52935618ad50c18309185937f
Erlan-Bam <erlanzh.gg@gmail.com>
2025-08-19 15:16:51 +0500

    feat: jwt

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './services/prisma.service';
 import { KafkaService } from './services/kafka.service';
 import { BillingService } from './services/billing.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 
 @Module({
-  providers: [PrismaService, KafkaService, BillingService],
-  exports: [PrismaService, KafkaService, BillingService],
+  imports: [JwtModule],
+  providers: [
+    PrismaService,
+    KafkaService,
+    BillingService,
+    JwtStrategy,
+    JwtService,
+  ],
+  exports: [
+    PrismaService,
+    KafkaService,
+    BillingService,
+    JwtStrategy,
+    JwtService,
+  ],
 })
 export class SharedModule {}
